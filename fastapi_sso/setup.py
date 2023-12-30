aae855575d7880ca898b916cecac713f8d4adf8c
Reinaldo Menendez <reinaldo@bloopark.de>
2023-12-30 12:28:24 +0100

    Add setup.py file

---
+++ fastapi_sso/setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='fastapi-sso',
+    packages=find_packages(),
+    version='1.0.0',
+    description='SSO for FastAPI',
+    author='rejamen@github',
+    license='BSD 3-Clause',
+)
